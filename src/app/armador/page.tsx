c48e425b3f0a155c24407945ae1aeaad09becd5f
Juan <genova.juan@alumnos.mdp.utn.edu.ar>
2024-01-16 19:05:41 -0300

    Agregar y mostrar Jugador y armado de equipos

--- src/app/armador/page.tsx
+++ src/app/armador/page.tsx
@@ -1,6 +1,7 @@
 import api from "@/api";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 import {
     Table,
     TableBody,
@@ -10,36 +11,50 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { Input } from "@/components/ui/input";
 
 export default async function BuilderPage() {
     const players = await api.player.list();
 
     async function createTeams(formData: FormData) {
-        "user server";
+        "use server";
 
         const players = formData.getAll("player");
-        console.log(players);
+
+        const searchParams = new URLSearchParams();
+
+        for (const player of players) {
+            searchParams.append("players", player as string);
+        }
+
+        redirect(`/armador/partido?${searchParams.toString()}`);
     }
     return (
-        <form action={createTeams} className="m-auto grid max-w-md gap-4">
-            <Table className="border">
-                <TableCaption>Tildar en caso de asistir</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Nombre</TableHead>
-                        <TableHead className="text-center"></TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {players.map(({ name }) => (
-                        <TableRow key={name}>
-                            <TableCell className="font-medium">{name}</TableCell>
-                            <TableCell className="text-center"><Checkbox name="player" value={name} /></TableCell>
+        <section className="m-auto grid max-w-md gap-4">
+            <form className="flex gap-4">
+                <Input name="player" placeholder="Nombre de jugador" />
+                <Button variant="secondary">Juega bonito, Sid</Button>
+            </form>
+            <form action={createTeams} className="grid gap-4" >
+                <Table className="border">
+                    <TableCaption>Tildar en caso de asistir</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Nombre</TableHead>
+                            <TableHead className="text-center"></TableHead>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <Button type="submit">Armar Equipos</Button>
-        </form>
+                    </TableHeader>
+                    <TableBody>
+                        {players.map(({ name }) => (
+                            <TableRow key={name}>
+                                <TableCell className="font-medium">{name}</TableCell>
+                                <TableCell className="text-center"><Checkbox name="player" value={name} /></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <Button type="submit">Armar Equipos</Button>
+            </form>
+        </section>
     )
 }
---
+++ src/app/armador/partido/page.tsx
@@ -0,0 +1,22 @@
+import api from "@/api";
+import { Player } from "@/types";
+
+export default async function MatchPage({
+    searchParams: { players: draft },
+}: {
+    searchParams: { players: string[] }
+}) {
+    const roster = await api.player.list();
+    const players = draft.map(
+        (name) =>
+            roster.find(player => player.name === name) ?? {
+                name,
+                matches: 0,
+                score: 0,
+            },
+    )
+
+    return (
+        <div>{JSON.stringify(players, null, 2)}</div>
+    );
+}
