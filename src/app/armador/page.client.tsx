977719c400a24053b95b2f564cb13dc34c2db634
Juan <genova.juan@alumnos.mdp.utn.edu.ar>
2024-01-17 16:11:59 -0300

    creo pageclient junto a la nueva funcionalidad de crear un nuevo jugador

---
+++ src/app/armador/page.client.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Player } from "@/types";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export default function BuilderPageClient({
+    players: initialPlayers
+    , onCreate
+}: {
+    players: Player[];
+    onCreate: (formData: FormData) => void
+}) {
+    const [players, setPlayers] = useState<Player[]>(initialPlayers);
+
+    function handleAddPlayer(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        setPlayers((players) =>
+            players.concat({ name: formData.get("player") as string, score: 0, matches: 0 }),
+        );
+    event.currentTarget.reset();
+    }
+
+return (
+    <section className="m-auto grid max-w-md gap-4">
+        <form className="flex gap-4" onSubmit={handleAddPlayer}>
+            <Input name="player" placeholder="Nombre de jugador" />
+            <Button variant="secondary">Juega bonito, Sid</Button>
+        </form>
+        <form action={onCreate} className="grid gap-4" >
+            <Table className="border">
+                <TableCaption>Tildar en caso de asistir</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead className="text-center"></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {players.map(({ name }) => (
+                        <TableRow key={name}>
+                            <TableCell className="font-medium">{name}</TableCell>
+                            <TableCell className="text-center"><Checkbox name="player" value={name} /></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Button type="submit">Armar Equipos</Button>
+        </form>
+    </section>
+)
+}
--- src/app/armador/page.tsx
+++ src/app/armador/page.tsx
@@ -1,23 +1,12 @@
-import api from "@/api";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/button";
-import { redirect } from "next/navigation";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { Input } from "@/components/ui/input";
+import  api  from '@/api';
+import BuilderPageClient from './page.client';
+import { redirect } from 'next/navigation';
 
-export default async function BuilderPage() {
+export default async function BuilderPage(){
     const players = await api.player.list();
-
+    
     async function createTeams(formData: FormData) {
-        "use server";
+        "use server"
 
         const players = formData.getAll("player");
 
@@ -29,32 +18,5 @@ export default async function BuilderPage() {
 
         redirect(`/armador/partido?${searchParams.toString()}`);
     }
-    return (
-        <section className="m-auto grid max-w-md gap-4">
-            <form className="flex gap-4">
-                <Input name="player" placeholder="Nombre de jugador" />
-                <Button variant="secondary">Juega bonito, Sid</Button>
-            </form>
-            <form action={createTeams} className="grid gap-4" >
-                <Table className="border">
-                    <TableCaption>Tildar en caso de asistir</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Nombre</TableHead>
-                            <TableHead className="text-center"></TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {players.map(({ name }) => (
-                            <TableRow key={name}>
-                                <TableCell className="font-medium">{name}</TableCell>
-                                <TableCell className="text-center"><Checkbox name="player" value={name} /></TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <Button type="submit">Armar Equipos</Button>
-            </form>
-        </section>
-    )
+    return <BuilderPageClient onCreate={createTeams} players={players} />
 }
