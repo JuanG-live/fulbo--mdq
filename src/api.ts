e21e0404cb1b5a166e0b3f45d565a6d603f97fe7
Juan <genova.juan@alumnos.mdp.utn.edu.ar>
2024-01-24 08:15:11 -0300

    Fix bug

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,11 @@
-import { Match } from './types';
-import { Player } from './types';
+import {Match} from './types';
+import {Player} from './types';
 
 const api = {
     match: {
         list: async (): Promise<Match[]> => {
             return fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQGuRjh8ZgUgJe9TWISFXqFisqIK1oWG9DOMu746Q-kywOfhT2bGSyGCisIY6GQqFZPPyYeE6Iovmmb/pub?output=tsv',
-                { next: { revalidate: 0 } }
+                {next: {tags: ["matches"]}},
             )
                 .then((res) => res.text())
                 .then((text) => {
