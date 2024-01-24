eefef10dd97622ec8bc9f91b587eeb1984c0bdde
Juan <genova.juan@alumnos.mdp.utn.edu.ar>
2024-01-24 18:50:07 -0300

    change google sheet

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import {Player} from './types';
 const api = {
     match: {
         list: async (): Promise<Match[]> => {
-            return fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQGuRjh8ZgUgJe9TWISFXqFisqIK1oWG9DOMu746Q-kywOfhT2bGSyGCisIY6GQqFZPPyYeE6Iovmmb/pub?output=tsv',
+            return fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQGuRjh8ZgUgJe9TWISFXqFisqIK1oWG9DOMu746Q-kywOfhT2bGSyGCisIY6GQqFZPPyYeE6Iovmmb/pubhtml',
                 {next: {tags: ["matches"]}},
             )
                 .then((res) => res.text())
